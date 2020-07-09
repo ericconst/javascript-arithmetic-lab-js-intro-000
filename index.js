e61156d680b1a263ef6d447f7b44b24faca48f9c
Eric C <erix2205@gmail.com>
2020-07-09 10:43:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,19 @@ function add(number1,number2){
 function subtract(a,b){
   return a-b
 }
+
+function multiply(a,b){
+  return a*b
+}
+
+function divide(a,b){
+  return a/b
+}
+
+function increment(n){
+  return n+=1
+}
+
+function decrement(n){
+  return n-=1
+}
